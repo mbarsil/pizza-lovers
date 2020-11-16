15100385ba26d7cdc77cfc88e8b64bda79a5c987
Mario Barrajon Silva <marb@ciklum.com>
2020-11-16 22:48:32 +0100

    feat(top-ten-chart): Renamed data props

--- pizza-lovers-client/src/components/top-ten-chart/top-ten-chart.tsx
+++ pizza-lovers-client/src/components/top-ten-chart/top-ten-chart.tsx
@@ -1,11 +1,6 @@
 import React, { FunctionComponent, useState } from 'react';
 
-import {
-  Chart,
-  ArgumentAxis,
-  ValueAxis,
-  BarSeries,
-} from '@devexpress/dx-react-chart-material-ui';
+import { Chart, ArgumentAxis, ValueAxis, BarSeries } from '@devexpress/dx-react-chart-material-ui';
 import { Paper } from '@material-ui/core';
 
 import { OwnProps, Props } from '../../shared/common.interface';
@@ -15,18 +10,14 @@ import { getStyles } from './top-ten-chart.styles.ts';
 const classNames = require('classnames');
 
 const data = [
-  { user: 'Mario', population: 2 },
-  { user: 'John', population: 3 },
-  { user: 'Paul', population: 3 },
-  { user: 'Joe', population: 4 },
-  { user: 'Kristen', population: 5 },
-  { user: 'Anne', population: 7 },
-  { user: 'Mario', population: 8 },
+  { userName: 'Mario', likes: 2 },
+  { userName: 'John', likes: 3 },
+  { userName: 'Paul', likes: 3 },
+  { userName: 'Joe', likes: 4 },
+  { userName: 'Kristen', likes: 5 },
+  { userName: 'Anne', likes: 7 },
+  { userName: 'Connor', likes: 8 },
 ];
-
-interface OwnState {
-  data: object[];
-}
 
 const TopTenChart: FunctionComponent<Props> = (props: OwnProps) => {
   const classes = getStyles();
@@ -38,8 +29,7 @@ const TopTenChart: FunctionComponent<Props> = (props: OwnProps) => {
       <Chart data={chartState}>
         <ArgumentAxis />
         <ValueAxis />
-
-        <BarSeries valueField="population" argumentField="user" />
+        <BarSeries valueField="likes" argumentField="userName" />
       </Chart>
     </Paper>
   );
